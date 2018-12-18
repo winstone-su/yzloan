cad7e04f768655374fa497386bd0d3c371088af2
carlsu <winstone.su@gmail.com>
2018-12-18 19:56:20 +0800

    initial

---
+++ yzloanTests/yzloanTests.m
@@ -0,0 +1,39 @@
+//
+//  yzloanTests.m
+//  yzloanTests
+//
+//  Created by 苏文彬 on 2018/12/18.
+//  Copyright © 2018年 yinzhong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yzloanTests : XCTestCase
+
+@end
+
+@implementation yzloanTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
